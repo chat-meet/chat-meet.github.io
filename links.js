7120a522d0a45cc687e421ee163336d7b13d089e
chat-meet <diana1s@myprofiles.live>
2026-09-13 23:17:41 +0600

    Add new redirect for lind-850

--- links.js
+++ links.js
@@ -1,3 +1,7 @@
 const REDIRECTS = {
-  "Diana-Hall-270": "https://uvt2mr8.light-meet.com/dy7tqc3?m=1&s1=dhall"
+
+  "Diana-Hall-270": "https://uvt2mr8.light-meet.com/dy7tqc3?m=1&s1=dhall",
+
+  "lind-850": "https://uvt2mr8.light-meet.com/dy7tqc3?m=1&s1=gtt"
+
 };
